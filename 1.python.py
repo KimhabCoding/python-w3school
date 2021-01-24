f1215ff06e48b97e3101b2db2efb8590cafc452a
KimhabCoding <kimhab.coding@gmail.com>
2021-01-24 10:49:48 +0700

    First Push Python

---
+++ 1.python.py
@@ -0,0 +1,31 @@
+if 5 > 2:{
+    print("Five is greater than two!")
+}
+
+if 5 > 2:
+    print("Five is greater than two! Version2")
+
+# This a python identation  
+# if 5 > 2:
+
+""" print("Five is greater than two! Version2")
+print("Five is greater than two! Version2") """
+
+x = 'String' 
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+print(type(x))
+
+# x1 = 'Kimhab'
+X1 = 'Kimhab X1'
+
+x2 = "Kimhab"
+print(X1)
+print(x2)
+# print(X1)
