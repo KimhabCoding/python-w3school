9cafa84b6dee2b99d16976bcb1f73348669616f6
KimhabCoding <kimhab.coding@gmail.com>
2021-01-27 06:06:55 +0700

    Add List Item Python

---
+++ 20.add-list-item.py
@@ -0,0 +1,13 @@
+list1 = ['Java', 'Python', 'PHP', 'HTML']
+# list1.append('CSS')
+# insert(index, 'Value')
+# list1.insert(4, 'CSS')
+# list.extend(list2)
+list2 = ['Djang', 'Laravel', 'Java Spring']
+# list1.extend(list2)
+list3 = ['Perl', 'C++', 'C']
+listtuple = ('Hkimhab', 28)
+list1.extend(listtuple)
+# list1.extend(list3)
+# print(list1)
+print(list1)
