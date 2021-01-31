f1fb80ad994dfeb2289c221143f403f42a45cc7c
KimhabCoding <kimhab.coding@gmail.com>
2021-01-31 08:58:59 +0700

    Exercise Python File and Image

---
+++ exercise/0.basic.py
@@ -0,0 +1,6 @@
+print('Hello My Crush')
+print('---*** I Love Programming do much ***---')
+# This a comment line in python 
+print("I am so happy today")
+print('Error with Semi colon'); # Error when you end with semi colon 
+print("Do you love your crush")
---
+++ exercise/1.python.py
@@ -0,0 +1,25 @@
+# True with identation  
+if 22 > 12:{
+    print("22 is bigger than 12")
+}
+
+# Indentation expected an indented block
+if 22 > 12:
+print('This wrong out here')
+    
+if 22 > 12:{
+print('This wrong out here')
+}
+
+# This is a comment line 
+""" This is a multi comment block line 
+    You can comment here too
+"""
+
+py1 = str(12)    # x will be '12'
+py2 = int(30)    # y will be 30
+py3 = float(40)  # z will be 40.0
+print(py1)
+print(py2)
+print(py3)
+print(type(py3))
