25fcfbdfcf2b4e638200fc95babeaf13611cc56c
KimhabCoding <kimhab.coding@gmail.com>
2021-01-31 08:58:26 +0700

    Remove List Items

---
+++ 21.remove-list-items.py
@@ -0,0 +1,15 @@
+# Remove List Items 
+ls = ['Phnom Penh', 'Apple', 'Chrome', 'Brave Browser', 'Dell']
+# ls.remove('Apple')
+# chrome != Chrome 
+# l2 = ls.remove('chrome')
+# print(ls.remove('Apple')) --> wrong 
+
+# pop(index)
+# pop() remove last item 
+# ls.pop(0) #['Apple', 'Chrome', 'Brave Browser']
+# ls.pop()
+# del list(index) = list.pop(index)
+# del ls[1] # list.pop(1)
+ls.clear()
+print(ls)
