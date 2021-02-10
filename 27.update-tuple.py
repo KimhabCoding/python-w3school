6a89affc4884dfb68d735f77084d0780da87fca9
KimhabCoding <kimhab.coding@gmail.com>
2021-02-10 22:55:48 +0700

    Update and Unpack Tuple

---
+++ 27.update-tuple.py
@@ -0,0 +1,15 @@
+tuplelist1 = ('Hack Facebook Crush', 'Love my Crush, Python', 'Studying Python')
+# Convert tuple to list 
+kh = list(tuplelist1)
+# Update Tuple
+kh[1] = 'Love you'
+# Add data to tuple 
+kh.append('Love IT')
+# Remove Item 
+kh.remove(kh[0]); 
+# Convert list to tuple 
+kh1 = tuple(kh)
+# Output Data 
+print(kh1)
+# Check Type 
+print(type(kh1))
---
+++ 28.unpack-tuple.py
@@ -0,0 +1,14 @@
+tuple1 = ("Django", "Hack Crush Facebook", "Love You", "Like Python")
+print(tuple1)
+print('---Unpack Tuple---')
+# Unpack Tule 
+# (var1, var2, var3) = tuple1
+""" print(var1) # == print(tuple1[0])
+print(var2)
+print(var3) """
+# Asterix * 
+(*var4, var5, var6) = tuple1 
+print(var4)
+print(var5)
+print(var6)
+print(type(var6))
