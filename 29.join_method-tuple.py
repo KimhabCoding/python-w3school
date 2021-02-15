5ff3cb4ad90ec32024f478892a4db4237af607c3
KimhabCoding <kimhab.coding@gmail.com>
2021-02-15 21:51:42 +0700

    Tuple Python

---
+++ 29.join_method-tuple.py
@@ -0,0 +1,18 @@
+# Join Tuple 
+tp1 = ('Do you love me', 'Do have crush', 'Hi, Crush')
+tp2 = (2, 3, 22, 12, 2, 2, 45, 2)
+tp3 = tp1 + tp2 
+print(tp3)
+print('Mul tuple')
+tp4 = tp1 *2 
+print(tp4)
+# Method Tuple 
+print('---Method Tuple---')
+tpm1 = tp2.count(2)
+print(tpm1)
+# Index 
+# Syntax: tuple.index(value)
+tpi1 = tp1.index('Do have crush')
+print(tpi1)
+print( 'tp2.index(2)') 
+print(tp2.index(2))
---
+++ 30.sets_basic.py
@@ -0,0 +1,21 @@
+var_sets = {'Hey Crush', 'Hack Crush', 'Do you like python', 'Love Django', 34, 12, False, True}
+vsets_single = {12, 34, 45}
+print(var_sets)
+print(vsets_single)
+# Unordered | No Duplicate data 
+# Check len 
+check_len = len(var_sets)
+print(check_len)
+# Check Type 
+check_type = type(vsets_single)
+print(check_type)
+# set() constructor 
+var_list = ('PHP', 'Love Speaker now')
+print(type(var_list))
+# Make var_list to set 
+print('Make var_list to set')
+# Syntax: set(variable)
+var_list_set = set(var_list)
+print(var_list_set)
+print('check type of var_list_set')
+print(type(var_list_set))
