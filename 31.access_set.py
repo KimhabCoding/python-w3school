fb2fe682f74fbd6e4491401f0a63ccf99be97b87
KimhabCoding <kimhab.coding@gmail.com>
2021-02-18 21:43:24 +0700

    Access Set Python

---
+++ 31.access_set.py
@@ -0,0 +1,11 @@
+set_var = {"Your Crush", "Love Your Crush", "Hack Baby", 12, False}
+# syntax loop 
+# for i in var 
+    # condition | output data 
+for l in set_var: 
+    print(l)
+
+# syntax check present 
+# print ("value" in variable)
+print(123 in set_var) #// False 
+ 
