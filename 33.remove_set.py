59021902a5a215f8b62744b9048996c7e2dd6273
KimhabCoding <kimhab.coding@gmail.com>
2021-02-20 22:14:36 +0700

    Remove set python

---
+++ 33.remove_set.py
@@ -0,0 +1,9 @@
+vset1 = {'HKimhab', 'N8', 'Hack Wing', 'Hack Facebook'}
+# vset1.discard('N812')
+# print(vset1)
+# pop, clear() , del 
+# vset1.pop()
+# syntax of del: del variable 
+# del vset1
+vset1 ="Kimhab"
+print(vset1)
